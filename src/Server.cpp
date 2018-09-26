920e37a88c79475164ab81be6ef18f3f9dfaa1b6
Jeff Cave <jcavejr@gmail.com>
2018-09-26 14:57:58 -0400

    Fixed comparing arguments

--- src/Server.cpp
+++ src/Server.cpp
@@ -1,11 +1,19 @@
 #include "ChatroomConfig.hpp"
 #include <iostream>
 #include "Server.hpp"
+#include <string>
 
 int main(int argc, char* argv[]) {
-    if (argv[1] == "-version" || argv[1] == "-v") {
-        std::cout << "Chatroom Version " << Chatroom_VERSION_MAJOR << "." << ChatroomVERSION_MINOR << endl;
+    std::string sargv[argc];
+    for (int i = 0; i < argc; i++) {
+        sargv[i] = argv[i];
+    }
+    if (sargv[1] == "-version" || sargv[1] == "-v") {
+        std::cout << "Chatroom Version " << Chatroom_VERSION_MAJOR << "." << Chatroom_VERSION_MINOR << std::endl;
         return 0;
+    } else {
+        std::cerr << "Error: Invalid invocation of Server" << std::endl;
+        return 1;
     }
     return 0;
 }
